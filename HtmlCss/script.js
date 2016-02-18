6799a8b28945c322b24c39bec5bed07c7a3df505
ericnjin <erickim000@gmail.com>
2016-02-18 10:27:20 +0900

    html practice

--- HtmlCss/script.js
+++ HtmlCss/script.js
@@ -1,28 +1,6 @@
-var ishEven = function(number) {
-  // Your code goes here!
-
-  var nanValue = number;
-  //if(nanValue != nanValue) // Returns true!
-    //alert('nanValue is NaN');.........................................................>!!!!!
-  var ret = isNaN(number);
-  
-  console.log(ret);
-  
-  if (nanValue != nanValue) {
-    // Do something..........................................................................>
-    return "It is NaN";
-
-  } else if (ret == true){
-    // ggggggggggggggggggggggg..............................................................>
-    return "It is string";
-
-  } 
-  else {    // Otherwise
-    // Do a third thing......................................................>
-    return "It's not string";
-  }
-
-  
+window.onload = function(){
+    var hw = document.getElementById('hw');
+    hw.addEventListener('click', function(){
+        alert('Hello world');
+    })
 }
-
-isEven(NaN);
